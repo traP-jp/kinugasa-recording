e3e969e1544b0fa924f2f81891d1ad2598ed61ee
comavius <comavius@proton.me>
2026-08-24 04:31:16 +0900

    re-init nix flakes

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "kinugasa-recording";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = [pkgs.structurizr-cli];
+        };
+      };
+    };
+}
